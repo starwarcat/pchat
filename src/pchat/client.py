9324bb24233410d42d45b0f33e13b7658d3f446b
Nibiru <starwarcat@gmail.com>
2014-01-09 03:04:15 +1100

    a little more

--- src/pchat/client.py
+++ src/pchat/client.py
@@ -1,13 +1,40 @@
 #! /usr/bin/env python
 
 import socket
+import threading
 
+class Client():
+    flag = True
+    c = None
+    
+    def __init__(self):
+        self.flag = True
+        self.c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.c.connect(("127.0.0.1", 9999))
+    
+    def receiver(self):
+        while self.flag:
+            msg = self.c.recv(1024)
+            if not msg:
+                continue
+            print "Server say:" + msg
+#         self.c.close()
+        
+    def sender(self):
+        while self.flag:
+            msg = raw_input("you:")
+            if(msg == "/quit"):
+                self.flag = False
+            self.c.sendall(msg)
+#         self.c.close()
+        
 if __name__=='__main__':
-    c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    c.connect(("127.0.0.1", 8888))
+    client = Client()
     
-    while True:
-        msg = raw_input(":")
-        c.sendall(msg)
-    
-    c.close()
+    rcv = threading.Thread(target=client.receiver)
+    sdr = threading.Thread(target=client.sender)
+    rcv.start()
+    sdr.start()
+    rcv.join()
+    sdr.join()
+    print 'End'
--- src/pchat/server.py
+++ src/pchat/server.py
@@ -8,7 +8,9 @@ class Server():
     
     login_socket = None
     host = "127.0.0.1"
-    port = 8888
+    port = 9999
+    conn = None
+    addr = None
     
     def __init__(self):
         self.login_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,12 +32,21 @@ class Server():
                     if not msg:
                         print "Disconnected..."
                         break
-                    print msg
+                    self.msgHandler(msg)
                 
             except Exception as e:
                 print "Unexpected error:", sys.exc_info()[0]    
-    
-    
+                
+    def msgHandler(self, msg):
+        if(msg=="/list"):
+            self.conn.send("Your Buddies are:192.168.0.13, 192.168.0.14 ...")
+            print 
+        elif(msg=="/quit"):
+            self.conn.close()
+        else:
+            print msg
+        
+
     
     
         
